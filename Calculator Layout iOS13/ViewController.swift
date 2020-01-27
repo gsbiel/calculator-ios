60f3bbd37dc2224dc12762753e4ec412b46b2221
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-01-27 13:15:11 -0500

    implementacao basica da logica de insercao de numeros na calculadora

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    // Propriedades do tipo "private" sao acessiveis apenas no escopo da classe onde estao declaradas.
+    // Assim, toda propriedade que for usada apenas como controle de estado dentro de uma classe pode ser declarada como privada, para evitar que sejam acessadas por instancias de outras classes.
+    private var isCalcButtonPressed : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isCalcButtonPressed = true
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if !isCalcButtonPressed {
+                if let label = displayLabel.text {
+                    displayLabel.text = label == "0" ? numValue : "\(displayLabel.text!)\(numValue)"
+                }
+            }else{
+                displayLabel.text = numValue
+                isCalcButtonPressed = false
+            }
+        }
+    }
 }
 
